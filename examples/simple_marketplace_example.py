e269916fdd57e9b21b5266335ef5d0a376a62184
aatamte <35645363+Aatamte@users.noreply.github.com>
2023-08-30 17:39:49 -0500

    update

---
+++ examples/simple_marketplace_example.py
@@ -0,0 +1,60 @@
+import os
+
+from CAES import Environment, Query, Order, Marketplace
+from CAES import Population
+from CAES import ActionRestriction
+from CAES import OAIAgent
+from CAES.artifacts.dialogue import Dialogue
+import openai
+
+
+class MyAgent(OAIAgent):
+    def __init__(self):
+        super(MyAgent, self).__init__()
+        self.inventory.set_starting_inventory(
+            {"capital": 1000, "socks": 10}
+        )
+
+        self.params["max_price"] = 10
+
+    def execute_query(self):
+        return Query()
+
+
+def buy_restriction(agent, order: Order):
+    assert order.quantity <= 0
+
+
+def sell_restriction(agent, order: Order):
+    assert order.quantity >= 0
+
+
+if __name__ == '__main__':
+    openai.api_key = os.environ["open_ai_key"]
+
+    env = Environment(visualization=True)
+
+    buyer_population = Population(agent=MyAgent(), number_of_agents=2)
+
+    seller_population = Population(agent=MyAgent(), number_of_agents=2)
+
+    env.add(buyer_population)
+    env.add(seller_population)
+
+    marketplace = Marketplace()
+    env.add(marketplace)
+
+    env.step_delay = 2
+
+    env.max_episodes = 1
+    env.max_steps = 50
+
+    # set up the environment
+    env.set_up()
+
+    for step in env.iter_steps():
+        for agent in env.agents:
+            print(agent.inventory)
+        print(step)
+        env.step()
+
