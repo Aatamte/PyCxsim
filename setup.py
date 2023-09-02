87b17eef104db607033fdb4aaf214cd45fd85433
aatamte <35645363+Aatamte@users.noreply.github.com>
2023-09-02 15:13:50 -0500

    update

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup, find_packages
 
+# Read the contents of your requirements.txt file
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='CAES',
+    version="0.1",
     packages=find_packages(where='src'),
+    install_requires=requirements,  # List of dependencies
     package_dir={'': 'src'},
     # other parameters...
 )
