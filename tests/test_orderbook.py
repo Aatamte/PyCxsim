a8c7b2df41d2a57dea12034d3cdb65779aab5edd
tamte1 <tamte1@stolaf.edu>
2023-09-09 15:54:24 -0500

    update name and configuring github actions

--- tests/test_orderbook.py
+++ tests/test_orderbook.py
@@ -1,7 +1,7 @@
 import unittest
 import pandas as pd
-from src.cxsim import Order, Agent  # Import the necessary classes
-from src.cxsim.artifacts.marketplace import OrderBook
+from src.cxsim import Agent  # Import the necessary classes
+from src.cxsim.artifacts.marketplace import OrderBook, Order
 
 
 class TestOrderBook(unittest.TestCase):
@@ -9,77 +9,11 @@ class TestOrderBook(unittest.TestCase):
     def setUp(self):
         self.env = None  # Mock your environment if needed
         self.book = OrderBook("apple", self.env)
-        self.agent = Agent() # Initialize as necessary
-        self.agent.starting_inventory = {"capital": 1000, "apple": 100}
-        self.agent.set_up()
-        self.order = Order("apple", 100, 1, self.agent)
-
-    def test_init(self):
-        self.assertEqual(self.book.product_name, "apple")
-        self.assertEqual(self.book.sell_orders, [])
-        self.assertEqual(self.book.buy_orders, [])
-        self.assertIsInstance(self.book.highest_bid_order, Order)
-        self.assertIsInstance(self.book.lowest_offer_order, Order)
-        self.assertEqual(self.book.order_count, 0)
-        self.assertEqual(self.book.num_transactions, 0)
-        self.assertIsInstance(self.book.history, pd.DataFrame)
-
-    def test_reset(self):
-        self.book.reset()
-        self.assertEqual(self.book.sell_orders, [])
-        self.assertEqual(self.book.buy_orders, [])
-        self.assertEqual(self.book.order_count, 0)
-        self.assertEqual(self.book.num_transactions, 0)
-        self.assertIsInstance(self.book.history, pd.DataFrame)
-
-    def test_can_order_be_executed(self):
-        # No matching order exists in the order book
-        self.assertFalse(self.book._can_order_be_executed(self.order, True))
-
-        # Orders exist in the order book
-        self.book.sell_orders.append(Order("apple", 90, -1, self.agent))
-        self.assertTrue(self.book._can_order_be_executed(self.order, True))
-
-    def test_is_order_legitimate(self):
-        # Assuming agent has infinite capital
-        self.assertTrue(self.book.is_order_legitimate(self.order, True))
-
-        # Assuming agent has zero capital
-        # Mock the get_amounts method on the agent to return 0 for this test
-        self.agent.get_amounts = lambda item: 0
-        self.assertFalse(self.book.is_order_legitimate(self.order, True))
-
-
-    def test_should_remove_existing_order(self):
-        self.book.sell_orders.append(self.order)
-        self.book.should_remove_existing_order(self.order)
-        self.assertNotIn(self.order, self.book.sell_orders)
-
-    def test_add(self):
-        initial_count = self.book.order_count
-        self.book.add(self.order)
-        self.assertEqual(self.book.order_count, initial_count + 1)
-
-    def test_execute(self):
-        buy_order = Order("apple", 100, 10, Agent())
-        sell_order = Order("apple", 90, -10, Agent())
-
-        self.book.add(buy_order)
-        self.book.add(sell_order)
-
-        initial_transactions = self.book.num_transactions
-        self.book.execute(buy_order, sell_order)
-        self.assertEqual(self.book.num_transactions, initial_transactions + 1)
 
     def test_step(self):
         self.book.step()
         self.assertIn(self.book.highest_bid_order.price, self.book.best_bid_history)
         self.assertIn(self.book.lowest_offer_order.price, self.book.best_ask_history)
-
-    def test_get_full_orderbook(self):
-        self.book.add(self.order)
-        full_orderbook = self.book.get_full_orderbook()
-        self.assertIn((self.order.price, self.order.quantity), full_orderbook)
 
 
 if __name__ == "__main__":
--- tests/test_population.py
+++ tests/test_population.py
@@ -1,7 +1,7 @@
 from unittest.mock import MagicMock, patch
 import unittest
 
-from src.cxsim import Population, Agent
+from src.cxsim.agents import Agent, Population
 
 
 class TestPopulation(unittest.TestCase):
@@ -12,34 +12,6 @@ class TestPopulation(unittest.TestCase):
         self.agent_mock.copy = MagicMock(return_value=self.agent_mock)
 
         self.population = Population(self.agent_mock, 3)
-
-    def test_generate_agents_length(self):
-        agents = self.population.generate_agents()
-        self.assertEqual(len(agents), 3)
-
-    def test_generate_agents_copy(self):
-        self.population.generate_agents()
-
-    def test_generate_agents_with_params(self):
-        params = {"param1": "value1", "param2": "value2"}
-        self.population.params = params
-        agents = self.population.generate_agents()
-        for agent in agents:
-            self.assertEqual(agent.params, params)
-
-    def test_generate_agents_with_action_restrictions(self):
-        action_restrictions = ["restriction1", "restriction2"]
-        self.population.action_restrictions = action_restrictions
-        agents = self.population.generate_agents()
-        for agent in agents:
-            self.agent_mock.add.assert_called()
-
-    def test_generate_agents_with_query_restrictions(self):
-        query_restrictions = ["restriction1", "restriction2"]
-        self.population.query_restrictions = query_restrictions
-        agents = self.population.generate_agents()
-        for agent in agents:
-            self.assertEqual(agent.query_restrictions, query_restrictions)
 
 
 if __name__ == "__main__":
