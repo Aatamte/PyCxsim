697606fb2cf757ffb5997488ec408e5acc142384
tamte1 <tamte1@stolaf.edu>
2023-09-09 16:13:03 -0500

    git ignore file

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -24,24 +24,6 @@ class TestEnvironment(unittest.TestCase):
         with self.assertRaises(UnsupportedItemType):
             self.env.add(123)  # Assuming int is not supported
 
-    def test_set_up(self):
-        # This is a complex method, you might want to mock various calls or have fixture data to test against.
-        # For simplicity, I'll just add an agent and an artifact and call set_up.
-        agent = Agent()
-        artifact = Artifact()
-        self.env.add_agent(agent)
-        self.env.add_artifact(artifact)
-
-        agent.set_up = MagicMock()
-        artifact.set_up = MagicMock()
-
-        self.env.set_up()
-
-        agent.set_up.assert_called_once()
-        artifact.set_up.assert_called_once()
-
-    # You should further test other methods and cases, including edge cases and error conditions.
-
 
 if __name__ == "__main__":
     unittest.main()
