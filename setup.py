44c980e92dc5cced0e3e781efcdb51cfa8ec0247
aatamte <35645363+Aatamte@users.noreply.github.com>
2023-10-24 17:32:57 -0500

    cleaning up files

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='pycxsim',
-    version="0.1.3",
+    version="0.1.4",
     license="MIT",
     author="Aaron Tamte",
     author_email="aaron2804@gmail.com",
@@ -15,7 +15,7 @@ setup(
     install_requires=requirements,  # List of dependencies
     description="PyCxsim is a framework to simulate computational agents in a confined environment.",
     url="https://github.com/Aatamte/PyCxsim",
-    keywords=["Artificial Intelligence"],
+    keywords=["Artificial Intelligence", "Simulation"],
     package_dir={'': 'src'},
     package_data={
         'cxsim': ['prompts/*.txt', 'gui/assets/*.ico'],
--- src/cxsim/agents/traits/memory/long_term_memory.py
+++ src/cxsim/agents/traits/memory/long_term_memory.py
@@ -1,5 +1,3 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
 
 
 class LongTermMemory:
